3e7255e6d742206d4992005c4e57f52034ea2c23
etrommer <elias_trommer@posteo.de>
2022-12-16 15:34:26 +0100

    Add MobileNetV2 and SqueezeNet templates

--- src/agnapprox/nets/mobilenetv2.py
+++ src/agnapprox/nets/mobilenetv2.py
@@ -17,11 +17,13 @@ class MobileNetV2(ApproxNet):
     approximate MobileNetV2
     """
 
-    def __init__(self, num_classes: int = 200, pretrained: bool = True):
-        super().__init__()
+    def __init__(self, num_classes: int = 200, pretrained: bool = True, **kwargs):
+        super().__init__(**kwargs)
 
         self.name = "MobileNetV2"
-        self.model = torchvision.models.mobilenet_v2(pretrained=pretrained)
+        self.model = torchvision.models.mobilenet_v2(
+            torchvision.models.MobileNet_V2_Weights.DEFAULT
+        )
 
         # Replace last layer with randomly initialized layer of correct size
         if num_classes != 1000:
@@ -31,21 +33,31 @@ class MobileNetV2(ApproxNet):
         self.epochs: dict = {
             "baseline": 30,
             "qat": 8,
-            "gradient_search": 5,
+            "noise": 2,
             "approx": 2,
         }
         self.num_gpus = 1
         self.gather_noisy_modules()
 
     def _baseline_optimizers(self):
-        optimizer = torch.optim.SGD(
-            self.parameters(), lr=1e-1, momentum=0.9, weight_decay=1e-4
-        )
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10)
+        # for p in self.model.parameters():
+        #     p.requires_grad = False
+        # self.model.classifier[1].requires_grad = True
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=2e-2)
+        scheduler = {
+            "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(
+                optimizer, factor=0.2, patience=3, mode="max"
+            ),
+            "monitor": "val_acc_top5",
+            "interval": "epoch",
+            "name": "lr",
+        }
         return [optimizer], [scheduler]
 
     def _qat_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=5e-3, momentum=0.9)
+        optimizer = torch.optim.Adam(
+            self.parameters(), lr=1e-3, momentum=0.9, weight_decay=1e-4
+        )
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 4)
         return [optimizer], [scheduler]
 
--- src/agnapprox/nets/squeezenet.py
+++ src/agnapprox/nets/squeezenet.py
@@ -27,11 +27,15 @@ class SqueezeNet(ApproxNet):
 
         # Replace last layer with randomly initialized layer of correct size
         if num_classes != 1000:
-            self.model.classifier[6] = torch.nn.Linear(4096, num_classes)
+            self.model.classifier[1] = torch.nn.Conv2d(
+                512, num_classes, kernel_size=(1, 1)
+            )
+            self.model.num_classes = num_classes
+            self.model.classifier[0] = torch.nn.BatchNorm2d(512)
 
         self.topk = (1, 5)
         self.epochs: dict = {
-            "baseline": 30,
+            "baseline": 35,
             "qat": 8,
             "noise": 2,
             "approx": 2,
@@ -40,14 +44,22 @@ class SqueezeNet(ApproxNet):
         self.gather_noisy_modules()
 
     def _baseline_optimizers(self):
-        optimizer = torch.optim.SGD(
-            self.parameters(), lr=1e-2, momentum=0.9, weight_decay=1e-4
-        )
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 10)
+        for p in self.model.parameters():
+            p.requires_grad = False
+        for p in self.model.classifier.parameters():
+            p.requires_grad = True
+        optimizer = torch.optim.Adam(self.model.classifier.parameters(), lr=1e-3)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 15)
         return [optimizer], [scheduler]
 
     def _qat_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=1e-3, momentum=0.9)
+        for n, p in self.model.named_parameters():
+            if "alpha" in n:
+                p.requires_grad = True
+            else:
+                p.requires_grad = False
+        params = [p for p in self.model.parameters() if p.requires_grad]
+        optimizer = torch.optim.Adam(params, lr=5e-5, weight_decay=1e-3)
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 4)
         return [optimizer], [scheduler]
 
